08ec6f171fe6608d33327bb9abce7fa61674f637
pauloborgesgh <pauloborgeszambon@gmail.com>
2022-08-29 19:27:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-qg58gq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qg58gq)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+5yryr
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
